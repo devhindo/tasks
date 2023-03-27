adaf3af6aaa795fad8ee62df861019f45fc856dd
hindawi <ahmedhindawi2001@gmail.com>
2023-03-27 02:48:23 +0200

    changed the way the tasks are handled

--- tasks.py
+++ tasks.py
@@ -5,9 +5,9 @@ tasks_folder = home_directory + "\\" + ".tasks"
 if not os.path.exists(tasks_folder):
     os.mkdir(tasks_folder)
     print(f"{tasks_folder} folder created successfully...")
+    tasks_file = tasks_folder + "\\" + "tasks.txt"
+    with open(tasks_file, "w") as f:
+        f.write("")
 else:
     print(f"{tasks_folder} folder already exists, delete it and try again...")
 
-tasks_file = tasks_folder + "\\" + "tasks.txt"
-with open(tasks_file, "w") as f:
-    f.write("")
